5c7cb9203673ead880fe60d60e5738dbea303f0f
Kado <kadodamball@hotmail.com>
2020-01-03 18:32:32 +0100

    Support listing a user's followers and starred repos

--- src/user.js
+++ src/user.js
@@ -13,4 +13,32 @@ const getUser = async function(req, username) {
   }
 }
 
+const getFollowers = async function(req, username) {
+  const options = Object.assign({
+    uri: `${ Config.API_BASE }/users/${ username }/followers?per_page=100`
+  }, Config.requestOptions);
+
+  try {
+    let followers = JSON.parse(await req(options));
+    return followers.map(follower => follower.login);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
+
+const getStarred = async function(req, username) {
+  const options = Object.assign({
+    uri: `${ Config.API_BASE }/users/${ username }/starred?per_page=100`
+  }, Config.requestOptions);
+
+  try {
+    let starredRepos = JSON.parse(await req(options));
+    return starredRepos.map(repo => repo.full_name);
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
+
 module.exports.getUser = getUser;
+module.exports.getFollowers = getFollowers;
+module.exports.getStarred = getStarred;
--- tests/unit/user.js
+++ tests/unit/user.js
@@ -2,18 +2,20 @@ const Mocks = require('../helpers/mocks');
 const User = require('../../src/user');
 const assert = require('assert');
 
-const expectedUser = {
-  username: 'mojombo',
-  repos_url: 'https://api.github.com/users/mojombo/repos'
-};
-
 describe(`#User's Github Info`, () => {
   it(`should return user's github information`, (done) => {
+    const expectedUser = {
+      username: 'mojombo',
+      repos_url: 'https://api.github.com/users/mojombo/repos',
+      created_at: '2012-01-20T02:28:06Z'
+    };
+
     let testUserData = async function(cb) {
       try {
         let user = await User.getUser(Mocks.getUser, `mojombo`);
         assert.equal(expectedUser.username, user.login, `Wrong user returned`);
         assert.equal(expectedUser.repos_url, user.repos_url, `Wrong repo url returned`);
+        assert.equal(expectedUser.created_at, user.created_at, `Wrong start date returned`);
         cb();
       } catch (err) {
         cb(err);
@@ -21,5 +23,35 @@ describe(`#User's Github Info`, () => {
     };
 
     testUserData(done);
+  });
+
+  it(`should return user's followers`, (done) => {
+    const expectedFollowers = ["tokuda109", "svallory", "weldan", "gavinuhma", "yijie", "filipediasf", "savekirk", "zhuyinan", "constraint", "scrx"];
+    let testUserFollowers = async function(cb) {
+      try {
+        let followers = await User.getFollowers(Mocks.getFollowers, `mojombo`);
+        assert.deepEqual(expectedFollowers, followers, `Wrong followers returned`);
+        cb();
+      } catch (err) {
+        cb(err);
+      }
+    };
+
+    testUserFollowers(done);
+  });
+
+  it(`should return user's starred repos`, (done) => {
+    const expectedRepos = ["prisma/lift", "prisma/prisma2", "prisma/photonjs", "hammerframework/hammer", "hammerframework/example-invoice"];
+    let testUserStarred = async function(cb) {
+      try {
+        let repos = await User.getStarred(Mocks.getStarred, `mojombo`);
+        assert.deepEqual(expectedRepos, repos, `Wrong repos returned`);
+        cb();
+      } catch (err) {
+        cb(err);
+      }
+    };
+
+    testUserStarred(done);
   });
 });
